79e21af641a1462a6715ae0344e6e7ebcd2ff735
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-05-13 11:48:33 +0530

    Make sure version command provide output to stdout with 0 return-code

--- cmd/machine-driver-libvirt/main.go
+++ cmd/machine-driver-libvirt/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/code-ready/machine-driver-libvirt"
 	"github.com/code-ready/machine/libmachine/drivers/plugin"
 )
 
 func main() {
+	if len(os.Args) > 1 {
+		if os.Args[1] == "version" {
+			fmt.Printf("Driver version: %s\n", libvirt.DriverVersion)
+			os.Exit(0)
+		}
+	}
 	plugin.RegisterDriver(libvirt.NewDriver("default", "path"))
 }
