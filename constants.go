0e78a57f97ed81422597bcfa9901e1ddb44678bb
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2019-05-16 11:14:49 +0530

    Issue #3 Change the libvirt dependency to lower version so it will not break.
    Right now we are building driver binary with latest release of libvirt and
    because of that it is breaking for the older system. Minikube also use
    older version of libvirt dep to create the driver binary
    
    - https://github.com/kubernetes/minikube/blob/master/go.mod#L43

--- constants.go
+++ constants.go
@@ -14,7 +14,7 @@ const (
 	DefaultIOMode    = "threads"
 	DefaultSSHUser   = "core"
 	DefaultSSHPort   = 22
-	DomainTemplate   = `<domain type='kvm' xmlns:qemu='http://libvirt.org/schemas/domain/qemu/1.0'>
+	DomainTemplate   = `<domain type='kvm'>
   <name>{{ .DomainName }}</name>
   <memory unit='MB'>{{ .Memory }}</memory>
   <vcpu placement='static'>{{ .CPU }}</vcpu>
@@ -39,52 +39,15 @@ const (
       <driver name='qemu' type='qcow2' cache='{{ .CacheMode }}' io='{{ .IOMode }}' />
       <source file='{{ .DiskPath }}'/>
       <target dev='vda' bus='virtio'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x05' function='0x0'/>
     </disk>
     <graphics type='vnc' autoport='yes' listen='127.0.0.1'>
       <listen type='address' address='127.0.0.1'/>
     </graphics>
-    <controller type='usb' index='0' model='piix3-uhci'>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x01' function='0x2'/>
-    </controller>
-    <controller type='pci' index='0' model='pci-root'/>
-    <controller type='virtio-serial' index='0'>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x04' function='0x0'/>
-    </controller>
     <interface type='network'>
       <mac address='52:fd:fc:07:21:82'/>
       <source network='{{.Network}}'/>
       <model type='virtio'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x03' function='0x0'/>
     </interface>
-    <serial type='pty'>
-      <target type='isa-serial' port='0'>
-        <model name='isa-serial'/>
-      </target>
-    </serial>
-    <console type='pty'>
-      <target type='serial' port='0'/>
-    </console>
-    <channel type='pty'>
-      <target type='virtio' name='org.qemu.guest_agent.0'/>
-      <address type='virtio-serial' controller='0' bus='0' port='1'/>
-    </channel>
-    <input type='mouse' bus='ps2'/>
-    <input type='keyboard' bus='ps2'/>
-    <graphics type='spice' autoport='yes'>
-      <listen type='address'/>
-    </graphics>
-    <video>
-      <model type='cirrus' vram='16384' heads='1' primary='yes'/>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x02' function='0x0'/>
-    </video>
-    <memballoon model='virtio'>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x06' function='0x0'/>
-    </memballoon>
-    <rng model='virtio'>
-      <backend model='random'>/dev/random</backend>
-      <address type='pci' domain='0x0000' bus='0x00' slot='0x07' function='0x0'/>
-    </rng>
   </devices>
 </domain>`
 )
--- go.mod
+++ go.mod
@@ -2,5 +2,5 @@ module github.com/code-ready/machine-driver-libvirt
 
 require (
 	github.com/code-ready/machine v0.7.1-0.20190501073902-87391ca2ceed
-	github.com/libvirt/libvirt-go v5.2.0+incompatible
+	github.com/libvirt/libvirt-go v3.4.0+incompatible
 )
